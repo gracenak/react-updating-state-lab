e3360b79da2eb0ee5a81d13c892aa6b2e7124074
gracenak <gracenak@gmail.com>
2021-03-14 23:53:07 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import { render } from 'enzyme'
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleButton = () => {
+        this.setState(previousState => {
+            return {
+              timesClicked: previousState.timesClicked + 1
+            }
+          })
+        }
+
+    render() {
+        return(
+            <button onClick={this.handleButton}>{this.state.timesClicked}</button>
+        )
+    }
+}
